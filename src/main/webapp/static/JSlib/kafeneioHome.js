b2cc68b61dbcbdd63c3b4ad275e6e61a0c7af228
Palak Gupta <palakgupta.biz@gmail.com>
2017-08-19 20:46:30 +0530

    Quantity Increment Logic added

--- src/main/webapp/static/JSlib/kafeneioHome.js
+++ src/main/webapp/static/JSlib/kafeneioHome.js
@@ -8,12 +8,14 @@ $( document ).ready(function() {
 	
     $("#invoiceGrid").jqGrid({
         colModel: [
+        	{ name: "foodCode", label: "Code", width: 140, align: "center",hidden:true},
             { name: "item", label: "Item", width: 140, align: "center" },
-            { name: "amount", label: "Amount", width: 100, template: "number" }
+            { name: "quantity", label: "Quantity", width: 80, align: "center" },
+            { name: "amount", label: "Amount", width: 80, template: "number"}
         ],
         data: [
-            {  name: "test",   amount: "600.88" },
-            {  name: "test2",  amount: "300.00" }
+           /* {  name: "test",   amount: "600.88" },
+            {  name: "test2",  amount: "300.00" }*/
            
         ],
         guiStyle: "bootstrap",
@@ -49,7 +51,11 @@ function writeDivsFromJson(data){
 	var html = '';
 	for (var i in data) {
 		var row = data[i];
-		html +='<div class="col-lg-2 div-item" onclick="addItem('+"'"+row.foodItemDesc+"'"+','+row.amount+')">'+row.foodItemDesc+'<br>'+ '\u20B9' +row.amount+'</div>';
+		html +='<div class="col-lg-2 div-item" onclick="addItem('
+			+"'"+row.id+"'"+','
+			+"'"+row.foodItemCode+"'"+','
+			+"'"+row.foodItemDesc+"'"+','
+			+row.amount+')">'+row.foodItemDesc+'<br>'+ '\u20B9' +row.amount+'</div>';
 	//alert(row.foodItemDesc,row.amount);
 	}
 	
@@ -65,7 +71,27 @@ function writeDivsFromJson(data){
 }
 
 
-function addItem(foodItemDesc,amount){
-	alert("add Item");
-	 $("#invoiceGrid").jqGrid("addRowData", 3, {  item:foodItemDesc ,  amount:amount  }, "last");
+function addItem(id,foodCode, foodItemDesc, amount){
+	//alert("add Item");
+	if(!increaseIfPresent(foodCode)){
+	 $("#invoiceGrid").jqGrid("addRowData",id , { foodCode:foodCode, item:foodItemDesc , quantity:1 ,  amount:amount  }, "last");
+		}
+	}
+	
+
+
+function increaseIfPresent(foodCode){
+	//alert(foodCode);
+	var isPresent=false;
+	var allData = $("#invoiceGrid").jqGrid("getGridParam", "data");
+	 $.each(allData, function(i, item){
+		 //alert(item.foodCode);
+		    if (item.foodCode == foodCode) {
+		    	isPresent=true;
+		    	var rowData = $("#invoiceGrid").jqGrid("getRowData", item.id);
+		    	rowData.quantity=parseInt(rowData.quantity)+1;
+		    	$("#invoiceGrid").jqGrid("setRowData", item.id, rowData);
+		    }
+		  });
+	 return isPresent;
 }
